9b58dde31426033c387170a26afb1db19b8c883d
monoronjonmodok <monoronjonmodok1@gmail.com>
2023-09-16 00:57:29 +0530

    first commit

---
+++ result.js
@@ -0,0 +1,31 @@
+
+const mark =22;
+switch(true){
+    case mark>100:
+        console.log("your mark is rong");
+        break;
+    case mark>=80:
+        console.log("you got: A+")
+        break;
+    case mark>=70:
+        console.log("you got: A")
+        break;
+    case mark>=60:
+        console.log ("you got: A-")
+        break;
+    case mark>=50:
+        console.log("you got: B")
+        break;
+    case mark>=40:
+        console.log("you got: C")
+        break;
+
+    case mark>=33:
+        console.log("you goy: D")
+        break;
+    case mark<33:
+        console.log("you are fail, valo kor por tahole pass korbi")
+        break;
+
+
+}
